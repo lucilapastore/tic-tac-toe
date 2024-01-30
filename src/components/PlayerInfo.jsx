0796964e602168e4012822ca52e244d5508a726f
Luli <lucilapastore@gmail.com>
2024-01-30 13:03:56 -0300

    feat: Editable player's names

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -1,19 +1,32 @@
 import { useState } from "react";
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
+
   function handleEditClick() {
-    setIsEditing((editing) => !editing);
+    setIsEditing((editing) => !editing); //Best practice way
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing === true) {
-    playerName = <input type="text" required value={name}></input>;
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange}
+      ></input>
+    );
   }
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
