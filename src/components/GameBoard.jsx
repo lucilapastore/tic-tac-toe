2049390cd09fdcfeebcc4da440485aaf5a81c770
Luli <lucilapastore@gmail.com>
2024-02-06 15:42:54 -0300

    fix: derive state from props

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,13 +1,18 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+  /* const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
@@ -19,7 +24,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
     });
 
     onSelectSquare();
-  }
+  } */
 
   return (
     <ol id="game-board">
@@ -28,7 +33,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
